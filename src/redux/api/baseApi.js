2940aaa2704d828d6f70f7fac93c879d506e701f
Md Likhon Mia <89283233+YSISLM104926@users.noreply.github.com>
2024-09-24 12:02:20 -0700

    Update baseApi.js

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
     reducerPath: 'baseApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://dmm-backend.vercel.app' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://dmm-backend-ahxp.onrender.com' }),
     tagTypes: ['Donation', 'Crisis', 'Status', 'Inventory', 'Expense'],
     endpoints: (builder) => ({
         getDonation: builder.query({
